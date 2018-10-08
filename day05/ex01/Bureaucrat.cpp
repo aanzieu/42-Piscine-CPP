6f2324cc22f82a60608d1107e6feb76fd57953ea
Anzieu Antoine <anzieu.antoine@gmail.com>
2018-10-08 20:18:42 +0200

    add day05

---
+++ day05/ex01/Bureaucrat.cpp
@@ -0,0 +1,164 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanzieu <aanzieu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 11:12:55 by aanzieu           #+#    #+#             */
+/*   Updated: 2018/10/08 19:39:28 by aanzieu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+//*************************//
+//****** BUREAUCRAT *******//
+//*************************//
+
+// Cannonic
+//------------------
+// Constructor void
+Bureaucrat::Bureaucrat(void)
+{
+    std::cout << "Create Bureaucrat" << std::endl;
+    return ;
+}
+// Constuctor return src (not deep)
+Bureaucrat::Bureaucrat(Bureaucrat const &src)
+{
+    *this = src;
+    return;
+}
+// Detroy constructor
+Bureaucrat::~Bureaucrat(void)
+{
+    std::cout << "Delete Bureaucrat" << std::endl;
+    return;
+}
+// Operator = return only grade, name is const
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rhs)
+{
+    this->_grade = rhs._grade;
+    return (*this);
+}
+//----Special Constructor -----//
+Bureaucrat::Bureaucrat(std::string const name, int grade) : _name(name)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    else if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    else
+        this->_grade = grade;
+    return;
+}
+
+// Functions GET
+//------------------
+// Get Name
+std::string Bureaucrat::getName(void) const
+{
+    return (this->_name);
+}
+// Get Grade
+int Bureaucrat::getGrade(void) const
+{
+    return (this->_grade);
+}
+
+// Operator Class
+//------------------
+// Operator +=
+void Bureaucrat::downgrade()
+{
+    if ((this->_grade - 1) < 1)
+        throw Bureaucrat::GradeTooLowException();
+    else
+        this->_grade -=  1;
+}
+// Operator -=
+void Bureaucrat::upgrade()
+{
+    if ((this->_grade + 1) > 150)
+        throw Bureaucrat::GradeTooHighException();
+    else
+        this->_grade += 1;
+}
+
+
+
+//**************************//
+//***** EXEPTION ERROR *****//
+//**************************//
+
+/*
+** GradeTool LOW **
+*/
+Bureaucrat::GradeTooLowException::GradeTooLowException(void)
+{
+	return;
+}
+Bureaucrat::GradeTooLowException::GradeTooLowException(GradeTooLowException const &src) throw()
+{
+	*this = src;
+	return;
+}
+Bureaucrat::GradeTooLowException::~GradeTooLowException(void) throw()
+{
+	return;
+}
+Bureaucrat::GradeTooLowException &Bureaucrat::GradeTooLowException::operator=(GradeTooLowException const &rhs) throw()
+{
+	(void)rhs;
+	if (this != &rhs) {}
+	return (*this);
+}
+// Function WHAT for Low
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Error: Grade too low (< 1)");
+}
+
+//---------------------------------------------------//
+
+/*
+** GradeTool HIGHT **
+*/
+Bureaucrat::GradeTooHighException::GradeTooHighException(void) throw()
+{
+    // std::cout << "Exeption Grade to Hight create " << std::endl;
+	return;
+}
+Bureaucrat::GradeTooHighException::GradeTooHighException(GradeTooHighException const &src) throw()
+{
+	*this = src;
+	return;
+}
+Bureaucrat::GradeTooHighException::~GradeTooHighException(void) throw()
+{
+	return;
+}
+Bureaucrat::GradeTooHighException &Bureaucrat::GradeTooHighException::operator=(GradeTooHighException const &rhs) throw()
+{
+    (void)rhs;
+    // std::cout << "Exeption Grade egale " << std::endl;
+	if (this != &rhs) {}
+	return (*this);
+}
+// Function WHAT for Hight
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Error: Grade too high (>150)");
+}
+
+//--------------------------------------//
+
+// Out Of Class
+//-----------------
+// Operator <<
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &rhs)
+{
+    o << COLOR_BLUE "<" << rhs.getName() << ">, bureaucrat grade <" << rhs.getGrade() << ">" << COLOR_RESET << std::endl;
+    return (o);
+}
---
+++ day05/ex01/main.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanzieu <aanzieu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 12:23:15 by aanzieu           #+#    #+#             */
+/*   Updated: 2018/10/08 20:18:00 by aanzieu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main()
+{
+	Bureaucrat zaz("Zaz", 5);
+	std::cout << "Bureaucrat zaz, grade 5" << std::endl
+			  << zaz;
+
+	Form form("42 First Intership", 7, 7);
+	std::cout << "Created Form" << COLOR_YELLOW << "<42 First Intership> " << COLOR_RESET << "with sign & exec requirement 7" << std::endl
+			  << form << std::endl;
+
+	std::cout << "Zaz try signing" << std::endl;
+	try
+	{
+		form.beSigned(zaz);
+
+		std::cout << std::endl
+				  << "Zaz try signing it again" << std::endl;
+		form.beSigned(zaz);
+		std::cout << std::endl
+				  << "Zaz don't remenber if he signed or not" << std::endl;
+		form.beSigned(zaz);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	std::cout << std::endl
+			  << "---------------------" << std::endl
+			  << std::endl;
+	std::cout << std::endl
+			  << "Create forms with 0 and 151 grade error catch" << std::endl;
+	int i = 0;
+	int x = 0;
+	while (i++ < 10)
+	{
+		try
+		{
+			if (i % 2 == 0)
+				x = 0;
+			else
+				x = 151;
+			Form form_next("Error", x, x);
+		}
+		catch (const std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl
+			  << "---------------------" << std::endl
+			  << std::endl;
+
+	std::cout << std::endl
+			  << "New Bureaucrat Ol with grade 100 and new form with requirement grade 50 or higher"
+			  << std::endl
+			  << std::endl;
+
+	try
+	{
+		Bureaucrat ol("Ol", 100);
+		std::cout << ol;
+
+		Form form3("42 Final Intership", 50, 50);
+		std::cout << form3;
+
+		form3.beSigned(ol);
+	}
+	// catch (Form::GradeTooHighException &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// catch (Form::GradeTooLowException &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
+}
